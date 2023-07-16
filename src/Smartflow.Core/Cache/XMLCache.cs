51d96bcd4c9e15a312e78f58634bf92d53b57804
CA228 <237552006@qq.com>
2023-07-16 17:42:29 +0800

    调整缓存

--- src/Smartflow.Core/Cache/XMLCache.cs
+++ src/Smartflow.Core/Cache/XMLCache.cs
@@ -7,10 +7,23 @@ using Smartflow.Core.Internals;
 
 namespace Smartflow.Core.Cache
 {
-    internal class XMLCache:ICache
+    internal class XMLCache : ICache
     {
         private readonly static IDictionary<long, IList<Node>> cache = new ConcurrentDictionary<long, IList<Node>>();
         private readonly static IWorkflowTemplateService templateService = WorkflowGlobalServiceProvider.Resolve<IWorkflowTemplateService>();
+
+        static XMLCache()
+        {
+            IList<WorkflowTemplate> templates = templateService.GetWorkflowTemplateList();
+            foreach (WorkflowTemplate template in templates)
+            {
+                if (!cache.ContainsKey(template.Id))
+                {
+                    Workflow workflow = XMLResolve.Parse(template.Source);
+                    cache.TryAdd(template.Id, workflow.Nodes);
+                }
+            }
+        }
 
         private static void Load(long templateId)
         {
@@ -22,41 +35,31 @@ namespace Smartflow.Core.Cache
             }
         }
 
-        public  void Publish(long templateId)
+        public void Publish(long templateId)
         {
             WorkflowTemplate template = templateService.GetWorkflowTemplateById(templateId);
             if (cache.ContainsKey(templateId))
             {
                 Workflow workflow = XMLResolve.Parse(template.Source);
                 cache[templateId] = workflow.Nodes;
-
             }
         }
 
-        public  Node GetNodeById(long templateId, string nodeId)
+        public Node GetNodeById(long templateId, string nodeId)
         {
             Load(templateId);
             IList<Node> nodes = cache[templateId];
             return nodes.Where(n => n.Id == nodeId).FirstOrDefault();
         }
 
-        public  IList<Node> GetNodesByTemplateId(long templateId)
+        public IList<Node> GetNodesByTemplateId(long templateId)
         {
             Load(templateId);
             return cache.ContainsKey(templateId) ? cache[templateId] : null;
         }
 
-        public  IDictionary<long, IList<Node>> GetAllTemplateNodes()
+        public IDictionary<long, IList<Node>> GetAllTemplateNodes()
         {
-            IList<WorkflowTemplate> templates = templateService.GetWorkflowTemplateList();
-            foreach (WorkflowTemplate template in templates)
-            {
-                if (!cache.ContainsKey(template.Id))
-                {
-                    Workflow workflow = XMLResolve.Parse(template.Source);
-                    cache.TryAdd(template.Id, workflow.Nodes);
-                }
-            }
             return cache;
         }
     }
